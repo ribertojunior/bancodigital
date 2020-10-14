c5d9ec82855cac4004ef17881ce750e07b58e119
Riberto Junior <ribertojunior@gmail.com>
2020-10-14 11:21:28 -0300

    Cliente controller restful

--- src/main/java/com/example/bancodigital/controller/ClienteController.java
+++ src/main/java/com/example/bancodigital/controller/ClienteController.java
@@ -3,9 +3,15 @@ package com.example.bancodigital.controller;
 import com.example.bancodigital.entity.Cliente;
 import com.example.bancodigital.repository.ClienteRepository;
 import lombok.AllArgsConstructor;
+import org.springframework.hateoas.CollectionModel;
+import org.springframework.hateoas.EntityModel;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.stream.Collectors;
+
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
 
 @RestController
 @AllArgsConstructor
@@ -13,8 +19,12 @@ public class ClienteController {
   private final ClienteRepository clienteRepository;
 
   @GetMapping("/clientes")
-  List<Cliente> all() {
-    return clienteRepository.findAll();
+  CollectionModel<EntityModel<Cliente>> all() {
+    List<EntityModel<Cliente>> clientes =
+        clienteRepository.findAll().stream().map(cliente -> EntityModel.of(cliente, linkTo(methodOn(ClienteController.class).one(cliente.getId())).withSelfRel(),
+        linkTo(methodOn(ClienteController.class).all()).withRel("clientes"))).collect(Collectors.toList());
+    return CollectionModel.of(clientes,
+        linkTo(methodOn(ClienteController.class).all()).withSelfRel());
   }
 
   @PostMapping("/clientes")
@@ -23,23 +33,33 @@ public class ClienteController {
   }
 
   @GetMapping("/clientes/{id}")
-  Cliente one(@PathVariable Long id) {
-    return clienteRepository.findById(id).orElseThrow(() -> new ClienteNotFoundException(id));
+  EntityModel<Cliente> one(@PathVariable Long id) {
+    Cliente cliente =
+        clienteRepository.findById(id).orElseThrow(() -> new ClienteNotFoundException(id));
+    return EntityModel.of(
+        cliente,
+        linkTo(methodOn(ClienteController.class).one(id)).withSelfRel(),
+        linkTo(methodOn(ClienteController.class).all()).withRel("clientes"));
   }
 
   @PutMapping("/clientes/{id}")
   Cliente replaceCliente(@RequestBody Cliente novoCliente, @PathVariable Long id) {
-    return clienteRepository.findById(id).map(cliente -> {
-      cliente.setNome(novoCliente.getNome());
-      cliente.setCpf(novoCliente.getCpf());
-      cliente.setDataDeNascimento(novoCliente.getDataDeNascimento());
-      cliente.setEmail(novoCliente.getEmail());
-      cliente.setSobrenome(novoCliente.getSobrenome());
-      return clienteRepository.save(cliente);
-    }).orElseGet(() -> {
-      novoCliente.setId(id);
-      return clienteRepository.save(novoCliente);
-    });
+    return clienteRepository
+        .findById(id)
+        .map(
+            cliente -> {
+              cliente.setNome(novoCliente.getNome());
+              cliente.setCpf(novoCliente.getCpf());
+              cliente.setDataDeNascimento(novoCliente.getDataDeNascimento());
+              cliente.setEmail(novoCliente.getEmail());
+              cliente.setSobrenome(novoCliente.getSobrenome());
+              return clienteRepository.save(cliente);
+            })
+        .orElseGet(
+            () -> {
+              novoCliente.setId(id);
+              return clienteRepository.save(novoCliente);
+            });
   }
 
   @DeleteMapping("/clientes/{id}")
